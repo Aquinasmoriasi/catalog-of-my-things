543aa371ef4af2ca39c69dab9cf6a1048fedc0d3
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-10-19 11:52:42 +0200

    Made required changes

--- music.rb
+++ music.rb
@@ -1,11 +1,13 @@
 class MusicAlbum < Item
   attr_accessor :on_spotify
+  attr_reader :name
 
   def initialize(publish_date, on_spotify)
     super(id, publish_date)
     @on_spotify = on_spotify
   end
 
+  private
   def can_be_archived?
     super && @on_spotify = true
   end
