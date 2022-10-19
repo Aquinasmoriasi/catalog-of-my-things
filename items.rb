cf4c35d551e7c401a00490984d3297714bcee7af
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-10-19 10:55:57 +0200

    Fixed linter errors

--- items.rb
+++ items.rb
@@ -1,23 +1,24 @@
 class Item
-    attr_reader :source, :author, :label, :publish_date, :genre
-    def initialize(author, source, label, publish_date)
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @id = Random.rand(1..100_000)
-        @genre = genre
-        @archived = false
-    end
-    def genre=(genre)
-        @genre = genre
-    end
-    def move_to_archive
-        @archieved = can_be_archieved?
-    end
+  attr_accessor :genre
+  attr_reader :source, :author, :label, :publish_date
 
-    private
-    def can_be_archived?
-        Date.today.year - Date.parse(@publish_date).year > 10
-    end
+  def initialize(author, source, label, publish_date)
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @id = Random.rand(1..100_000)
+    @genre = genre
+    @archived = false
+  end
+
+  def move_to_archive
+    @archieved = can_be_archieved?
+  end
+
+  private
+
+  def can_be_archived?
+    Date.today.year - Date.parse(@publish_date).year > 10
+  end
 end
--- music.rb
+++ music.rb
@@ -1,13 +1,12 @@
 class MusicAlbum < Item
-    attr_accessor :on_spotify
-    
-    def initialize(publish_date, on_spotify)
-        super(id, publish_date)
-        @on_spotify = on_spotify
-    end
+  attr_accessor :on_spotify
 
-    def can_be_archived?
-        super && @on_spotify = true
-    end
-  
+  def initialize(publish_date, on_spotify)
+    super(id, publish_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify = true
+  end
 end
