f3567dcbe2c50edb50379959f3032a67d1649731
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-10-21 09:56:29 +0200

    Preserved the music

--- modules/music_module.rb
+++ modules/music_module.rb
@@ -1,30 +1,36 @@
 require_relative '../music'
 require_relative '../genre'
 
-def add_album
-  print 'Enter album Name: '
-  name = gets.chomp
-  print 'Enter album genre: '
-  genre = gets.chomp
-  print 'Enter publish date: '
-  publish_date = gets.chomp
-  print 'is it on Spotify? [Y/N]: '
-  on_spotify = gets.chomp.downcase
-  on_spotify = on_spotify == 'y'
-  MusicAlbum.new(name, publish_date, on_spotify)
-  new_genre = Genre.new(genre)
+module MusicModule
+  def add_album
+    print 'Enter album Name: '
+    name = gets.chomp
+    print 'Enter album genre: '
+    genre = gets.chomp
+    print 'Enter publish date: '
+    publish_date = gets.chomp
+    print 'is it on Spotify? [Y/N]: '
+    on_spotify = gets.chomp.downcase
+    on_spotify = on_spotify == 'y'
+    MusicAlbum.new(name, publish_date, on_spotify)
+    new_genre = Genre.new(genre)
 
-  music_struct = MusicStruct.new(name: name, genre: genre, publish_date: publish_date,
-                                 on_spotify: on_spotify)
-  json = JSON.generate(music_struct)
-  @musics << json
-  File.write('musics.json', @musics)
+    music_struct = ItemStruct.new(name: name, genre: genre, publish_date: publish_date,
+                                  on_spotify: on_spotify)
+    json = JSON.generate(music_struct)
+    @musics << json
+    File.write('musics.json', @musics)
 
-  @genres << new_genre
-  puts '*************** Music add successfully!!***************'
-end
+    @genres << new_genre
+    puts '*************** Music add successfully!!***************'
+  end
 
-def list_all_genres
-  p @genres
-  puts ''
+  def list_all_genres
+    p @gernes
+  end
+
+  def list_all_music_albums
+    @musics = JSON.parse(File.read('musics.json')) if File.exist?('musics.json') && File.read('musics.json') != ''
+    p @musics
+  end
 end
